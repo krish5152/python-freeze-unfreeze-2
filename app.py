6c45615509f169fb16e2f2609f646212447f6cbe
naveen <naveenkumar1297g@gmail.com>
2024-10-01 17:21:30 +0530

    new66

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world6'
+    return 'Hello, world66'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)  # You can use debug mode for development new
